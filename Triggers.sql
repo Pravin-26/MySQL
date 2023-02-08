b66f1eb7a51f5a94f1dbff05cda7a210a178e366
Pravin Shinde <ps07375n@pace.edu>
2023-02-08 00:11:52 -0500

    Trigger update

---
+++ Triggers.sql
@@ -0,0 +1,37 @@
+/* CREATE A DATABASE */
+CREATE DATABASE productdata;
+
+/* use database */
+USE productdata;
+
+/* Create Table */
+CREATE TABLE sampletable(id int, samplename varchar(50), cost float, count int);
+
+
+/* Inserting values */
+INSERT INTO sampletable(id int, samplename varchar(50), cost float, count int) VALUES (1, 'Apple', 50, 100 );
+
+INSERT INTO sampletable(id int, samplename varchar(50), cost float, count int) VALUES (2, 'Banana', 100, 100 );
+
+INSERT INTO sampletable(id int, samplename varchar(50), cost float, count int) VALUES (3, 'Orange', 150, 100 );
+
+INSERT INTO sampletable(id int, samplename varchar(50), cost float, count int) VALUES (4, 'Pineapple', 200, 100 );
+
+INSERT INTO sampletable(id int, samplename varchar(50), cost float, count int) VALUES (5, 'Mango', 250, 100 );
+
+/* Retreiving records from Table */
+SELECT * FROM sampletable;
+
+
+/* Process - Creating a Trigger */
+DELIMITER //
+
+/* Logic is triggered before inserting column in sampletable for cost price which is less than 0 */
+CREATE Trigger costcheckforsamplename BEFORE INSERT ON sampletable FOR EACH ROW 
+BEGIN
+IF new.cost < 0 THEN  new.cost = 0;
+END IF;
+
+END //
+
+DELIMITER ;
