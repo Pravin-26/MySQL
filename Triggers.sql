13668587884d9dc70e498224f80a83866f442f3f
Pravin Shinde <ps07375n@pace.edu>
2023-02-08 01:48:51 -0500

    new commit

--- Triggers.sql
+++ Triggers.sql
@@ -5,19 +5,19 @@ CREATE DATABASE productdata;
 USE productdata;
 
 /* Create Table */
-CREATE TABLE sampletable(id int, samplename varchar(50), cost float, count int);
+CREATE TABLE sampletable(id int, samplename varchar(50), cost float, stock int);
 
 
 /* Inserting values */
-INSERT INTO sampletable(id int, samplename varchar(50), cost float, count int) VALUES (1, 'Apple', 50, 100 );
+INSERT INTO sampletable(id int, samplename varchar(50), cost float, stock int) VALUES (1, 'Apple', 50, 100 );
 
-INSERT INTO sampletable(id int, samplename varchar(50), cost float, count int) VALUES (2, 'Banana', 100, 100 );
+INSERT INTO sampletable(id int, samplename varchar(50), cost float, stock int) VALUES (2, 'Banana', 100, 100 );
 
-INSERT INTO sampletable(id int, samplename varchar(50), cost float, count int) VALUES (3, 'Orange', 150, 100 );
+INSERT INTO sampletable(id int, samplename varchar(50), cost float, stock int) VALUES (3, 'Orange', 150, 100 );
 
-INSERT INTO sampletable(id int, samplename varchar(50), cost float, count int) VALUES (4, 'Pineapple', 200, 100 );
+INSERT INTO sampletable(id int, samplename varchar(50), cost float, stock int) VALUES (4, 'Pineapple', 200, 100 );
 
-INSERT INTO sampletable(id int, samplename varchar(50), cost float, count int) VALUES (5, 'Mango', 250, 100 );
+INSERT INTO sampletable(id int, samplename varchar(50), cost float, stock int) VALUES (5, 'Mango', 250, 100 );
 
 /* Retreiving records from Table */
 SELECT * FROM sampletable;
@@ -32,6 +32,20 @@ BEGIN
 IF new.cost < 0 THEN  new.cost = 0;
 END IF;
 
+END //
+
+DELIMITER ;
+
+
+/*  <===============================================================================================> */
+
+
+/* Recurring Event to check count of Products  */
+
+DELIMITER //
+
+CREATE EVENT countcheck ON SCHEDULE EVERY 1 DAY DO
+BEGIN IF sampletable.stock < 50 THEN UPDATE sampletable WHERE  stock = 100; END IF;
 END //
 
 DELIMITER ;
